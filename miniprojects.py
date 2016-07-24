f9f49e07cb00c4c8bbc2d1b487b0aef6e5806cc6
Jobin <maluplaya08@gmail.com>
2016-07-23 22:07:20 -0500

    up to date last 1007pm

--- miniprojects.py
+++ miniprojects.py
@@ -1,30 +1,31 @@
 import json
 import random
-from IPython.display import Image, display
 
 def question1(s, t):
-    for val in range(len(t)):
-    	found = True
-    	if t[val] not in s:
-    		found= False
-    		break
-    	else:
-    		count = 0
-    		ss = list(s)
-    		for l in ss:
-    			count+=1 
-    			if l==t[val]:
-    			 	del ss[count]
-    			 	break
-    return found
+	return True if (t[::-1] in s or t in s) else False
+	# for val in range(len(t)):
+	# 	found = True
+	# 	if t[val] not in s:
+	# 		found= False
+	# 		break
+	# 	else:
+	# 		count = 0
+	# 		ss = list(s)
+	# 		for l in ss:
+	# 			count+=1 
+	# 			if l==t[val]:
+	# 				del ss[count]
+	# 				break
+	# return found
 
 def question2(a):
     possibleChoices = []
     for j in range(0,len(a)+1):
         for i in range(0,len(a)+1):
-            if len(str(a[j:i]))>1 and str(a[j:i]) == str(a[j:i])[::-1]:
+            if len(str(a[j:i]))>0 and str(a[j:i]) == str(a[j:i])[::-1]:
             	possibleChoices.append(str(a[j:i]))
     return max(possibleChoices, key=len) if possibleChoices else 'none found'
+
 
 def question3(G):
 	chosenVertex = []
@@ -45,26 +46,27 @@ def question3(G):
 	                            [val[0] for key in s32 for val in s32[key]])))
 	while(chosenVertex != completeVertexes):   
 	    for val in weightTable:
-	        if val[1] in chosenVertex or val[2] in chosenVertex:
+	        if val[1] in chosenVertex or val[2] in chosenVertex: # not a != (not b)
 	            if val[1] not in chosenVertex or val[2] not in chosenVertex:
 	                if val not in chosenEdges:
 	                    chosenEdges.append(val)
 	                chosenVertex.append(val[2] if val[2] not in chosenVertex else val[1])
 	                chosenVertex = sorted(chosenVertex)
 	                break
-        
-	#print 'chosenEdges = {} \nchosenVertex = {}'.format(chosenEdges, chosenVertex)
-	#print chosenVertex
-	#print chosenEdges
+	print 'chosenVertex = {},\nchosenEdges = {}'.format(chosenVertex,chosenEdges)
 	answer = {}
 	for val in chosenVertex:
-	    for edge in chosenEdges:
-	        if val in edge:
-	            answer[val] = (edge[2] if val!= edge[2] else edge[1],edge[0])
+		print val
+		for edge in chosenEdges:
+			if val in edge:
+				if val in answer:
+					answer[val].append((edge[2] if val!= edge[2] else edge[1],edge[0]))
+				else:
+					answer[val] = [(edge[2] if val!= edge[2] else edge[1],edge[0])]
 	return answer    
 
 
-def question4(T, r, n1, n2):
+def question4(T, r, n1, n2): # question4(tree, 4,1,12)
 	class Node:
 	    def __init__(self,val):
 	        self.value = val
@@ -72,7 +74,7 @@ def question4(T, r, n1, n2):
 	        self.rightChild = None
 	    def insert(self, data):
 	        if self.value == data:
-	            return False
+	            return False	
 	        elif self.value > data:
 	            if self.leftChild: 
 	                return self.leftChild.insert(data)
@@ -100,15 +102,32 @@ def question4(T, r, n1, n2):
 	        for i,val in enumerate(tree[r-1]):
 	            if val == 1: 
 	                return self.table(i)
+
+	# def getChildren(Tree, node):
+	#     # read the row in the Tree that belongs to the node, checking each spot for a child
+	#     for position in Tree[node]:
+	#         if position == 1:
+	#             # you found a child node, now insert it!
 	
+	# def getChildren(T,r):
+	# 	tree = Tree(r)
+	# 	# read the row in the Tree that belongs to the node, checking each spot for a child
+	# 	for row in T:
+	# 		for i, position in enumerate(row):
+	# 			if position == 1:
+	# 				tree.insert(i)
+	#getChildren(T,r)
+
+	theTree = Tree()
+	theTree.table(r)
+
 	def lca(root, n1, n2):
-		if( root > max(n1, n2)):
+		if( root > n1 and root > n2):
 			return lca(root.left, n1, n2)
-		elif(root < min(n1, n2)):
+		elif(root < n1 and root < n2):
 			return lca(root.right, n1, n2)
 		else:
 			return root
-
 def question5(ll,m): 
 	class Node(object):
 
@@ -172,27 +191,33 @@ def question5(ll,m):
 
 	linked = LinkedList()
 	linked.makeLinked(ll)
-	return linked.getMth(m)        
+	return linked.getMth(m)
 
 def main():
 	## Functions
 	#print question1(s,t)
 	#print question2(word)
 	#print question3(s32)
-	#question4(tree, 4,1,12)
+	question4(tree, 4,1,12)
+	# makeLinked = LinkedList()
+	# headNode = makeLinked.getHeadNode()
 	#print question5(ll,m) # ll (first node is 3), m mth num from end
+
 
 
 
 ## Variables 
 # for # 1
 s = 'udacityy'
-t = 'yyu'
+t = 'ty'
 # for # 2
 #word = 'kayakhello'
 word = 'kayak'
-# for # 4
+word = 'notaplindrome'
+word = 'a'
+# for # 3 
 s32 = {'A':[('E',5),('H',6),('F',1),('B',8),('B',8)],'B':[('F',6),('C',4)],'C':[('F',2),('G',7)],'G':[('F',9)],'F':[('H',5)],'H':[('E',3)]}
+# for # 4
 tree = [[0,1,0,0,0,0,0,0,0,0],
 		[1,0,1,1,0,0,0,0,0,0],
 		[0,1,0,0,0,0,0,0,0,0],
